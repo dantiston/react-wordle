4d8a274ab64030a7402b06369f3427dce06491fd
T.J. Trimble <trimblet@me.com>
2022-01-22 09:06:03 -0800

    basic UI

---
+++ src/Board.js
@@ -0,0 +1,23 @@
+// @flow-strict
+
+import Container from 'react-bootstrap/Container';
+import Row from 'react-bootstrap/Row';
+import Col from 'react-bootstrap/Col';
+
+const width = 5;
+const height = 6;
+
+const letters = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'];
+
+const Board = () =>
+(<Container className='board' fluid='md'>
+    {[...Array(height).keys()].map(
+        i => <Row className='row' key={`row-${i}`}>{[...Array(width).keys()].map(
+            j => <Col key={`cell-${width * i + j}`}>
+                <div className='cell'>{letters[(width * i + j) % letters.length]}</div>
+            </Col>
+        )}</Row>
+    )}
+</Container>);
+
+export default Board;
